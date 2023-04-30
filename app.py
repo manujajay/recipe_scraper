93044590af667e9722a50f3537c0def6076fabb9
manujajay <manujajayawardanais@gmail.com>
2023-04-30 16:27:22 +0100

    added cart functionality

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, url_for, redirect
 import requests
-from bs4 import BeautifulSoup, Comment
-import re
-
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -14,29 +13,50 @@ def index():
         return render_template('index.html', ingredients=ingredients)
     return render_template('index.html')
 
+
+@app.route('/cart', methods=['GET', 'POST'])
+def cart():
+    if request.method == 'POST':
+        item = request.form['item']
+        if 'cart_items' not in session:
+            session['cart_items'] = []
+        session['cart_items'].append(item)
+        session.modified = True
+        return redirect(url_for('cart'))
+
+    cart_items = session.get('cart_items', [])
+    return render_template('cart.html', cart=cart_items)
+
 def scrape_ingredients(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Find the ingredients container
     ingredients_container = soup.find('section', class_='recipe__ingredients')
 
     if not ingredients_container:
         return []
 
-    # Extract ingredients and quantities from the container
     ingredients = []
     for li in ingredients_container.find_all('li', class_='list-item'):
-        # Remove HTML comments
-        for comment in li.find_all(text=lambda text: isinstance(text, Comment)):
-            comment.extract()
-
-        ingredient_text = li.text.strip()
+        ingredient_text = ' '.join(li.stripped_strings)
         if ingredient_text:
             ingredients.append(ingredient_text)
 
     return ingredients
 
+@app.route('/cart/count', methods=['GET'])
+def cart_count():
+    cart_items = session.get('cart_items', [])
+    return str(len(cart_items))
+
+@app.route('/delete_from_cart', methods=['POST'])
+def delete_from_cart():
+    item = request.form['item']
+    cart_items = session.get('cart_items', [])
+    if item in cart_items:
+        cart_items.remove(item)
+        session.modified = True
+    return redirect(url_for('cart'))
+
 if __name__ == '__main__':
     app.run(debug=True)
-
